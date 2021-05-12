813cef4068fdadf5bdbe3eb4f9df9c397370e44d
João Protásio da Luz Junior <joaoprotasiojr@gmail.com>
2021-05-12 02:40:32 -0300

    Primeiro código
    Meu primeiro código em C#

---
+++ cursocsharp/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cursocsharp {
+    class program {
+
+        static void Main(String[] args) {
+
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
