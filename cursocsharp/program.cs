66c4f64f86409179f3f4b459e0044179a803c5b9
João Protásio da Luz Junior <joaoprotasiojr@gmail.com>
2021-05-13 00:53:08 -0300

    Comandos do VSCode
    Alguns comandos do VSCode

--- cursocsharp/program.cs
+++ cursocsharp/program.cs
@@ -1,12 +1,23 @@
-using System;
+﻿using System;
+using System.Globalization;
 
-namespace cursocsharp {
-    class program {
+namespace cursocsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            double altura;
+            char sexo;
 
-        static void Main(String[] args) {
+            nome = Console.ReadLine();
+            idade = int.Parse(Console.ReadLine());
+            altura = double.Parse(Console.ReadLine());
+            sexo = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Olá Mundo!");
-            Console.ReadLine();
+            Console.Write("Seu nome é " + nome + " ele tem " + idade + " anos de idade, mede " + altura + " de altura e seu sexo é: " + sexo);
         }
     }
 }
